cb69b9a87f8e4ae38f61abdbd43a17bf81e0b7af
Andrey Helldar <helldar@ai-rus.com>
2018-10-08 17:29:56 +0300

    Comments has been added to model

--- src/Models/ErrorNotification.php
+++ src/Models/ErrorNotification.php
@@ -5,6 +5,20 @@ namespace Helldar\NotifyExceptions\Models;
 use Helldar\NotifyExceptions\Traits\FlattenException;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Helldar\NotifyExceptions\Models\ErrorNotification
+ *
+ * @property int $id
+ * @property string $parent
+ * @property \Exception $exception
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\AVANGARD\Models\Common\MetaTag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\AVANGARD\Models\Common\MetaTag whereException($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\AVANGARD\Models\Common\MetaTag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\AVANGARD\Models\Common\MetaTag whereParent($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\AVANGARD\Models\Common\MetaTag whereUpdatedAt($value)
+ */
 class ErrorNotification extends Model
 {
     use FlattenException;
@@ -23,6 +37,11 @@ class ErrorNotification extends Model
         $this->attributes['exception'] = serialize($value);
     }
 
+    /**
+     * @param string $value
+     *
+     * @return \Exception
+     */
     protected function getExceptionAttribute($value)
     {
         return unserialize($value);
