549430549e64fa1203a2ae8beb75c55d580150de
Andrey Helldar <helldar@ai-rus.com>
2018-10-08 15:43:41 +0300

    Fix migration class name

--- src/migrations/2018_07_11_140701_create_error_notifications_table.php
+++ src/migrations/2018_07_11_140701_create_error_notifications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ErrorNotification extends Migration
+class CreateErrorNotificationsTable extends Migration
 {
     public function up()
     {
